a7268544e30bb4b630b59603037951f7513d9c37
Assessor-123 <113010265+dipika-6666@users.noreply.github.com>
2024-08-03 13:15:24 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+// Optional JavaScript for more control over scrolling
+document.querySelectorAll('nav ul li a').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        const target = document.querySelector(this.getAttribute('href'));
+        target.scrollIntoView({ behavior: 'smooth', block: 'start' });
+    });
+});
